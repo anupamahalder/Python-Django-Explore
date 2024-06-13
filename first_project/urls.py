d41744c048fb97a0157c2571ddfd95fa2453f239
anupamahalder <anupamahalder2022@gmail.com>
2024-06-14 00:29:20 +0530

    Added home and contact page by navigating url

---
+++ first_project/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path
+# from views import contact
+from . import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('contact/', views.contact)
+]

5ffb4dc5638e0a7ed07463f6ba5f71d52d26dcf1
anupamahalder <anupamahalder2022@gmail.com>
2024-06-15 00:44:38 +0530

    Added app to the the project

--- first_project/urls.py
+++ first_project/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 # from views import contact
 from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('contact/', views.contact)
+    path('contact/', views.contact),
+    path('', views.home),
+    path('first_app/', include("first_app.urls")),
 ]

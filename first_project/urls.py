5b6907b76e71c3a9a8a62f8262961bd8f46d361b
anupamahalder <anupamahalder2022@gmail.com>
2024-06-23 12:29:01 +0530

    Loaded HTML template on home page

--- first_project/urls.py
+++ first_project/urls.py
@@ -6,7 +6,7 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('contact/', views.contact),
-    path('', views.home),
+    path('', views.index),
     # All the urls of first_app will start by first_app/
     path('first_app/', include("first_app.urls")),
     path('second_app/', include("second_app.urls")),
--- first_project/views.py
+++ first_project/views.py
@@ -1,4 +1,6 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+
 def contact(request):
     return HttpResponse("This is contact page!")
 
@@ -7,3 +9,6 @@ def home(request):
 
 def about(request):
     return HttpResponse("This is about page!")
+
+def index(request):
+    return render(request,'index.html')
